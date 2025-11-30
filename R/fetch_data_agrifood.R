7565dc580791bdfc398aa1744bd4c62771d54de9
Javier Ribal del Río <javierribal@gmail.com>
2025-11-30 10:55:25 +0100

    feat: script to fetch_data

--- R/fetch_data_agrifood.R
+++ R/fetch_data_agrifood.R
@@ -6,112 +6,153 @@
 
 library(jsonlite)
 library(dplyr)
+library(tools)
 
 options(timeout = 600)
 
-# Imports -----------------------------------------------------------------
+# Ciclos fiscales deseados
+ciclos_fiscales <- c("2021/2022", "2022/2023", "2023/2024")
 
-nombres_archivos_import <- c(
-  "import_oilseeds.json",
-  "import_cereals.json",
-  "import_olive_oil.json",
-  "import_wine.json",
-  "import_sugar.json",
-  "import_citrus_fruit.json"
-)
+# Sectores (formato URL correcto)
+sectores <- c("Oilseeds", "Cereals", "Olive%20Oil", "Wine", "Sugar", "Citrus%20Fruit")
 
-nombres_url_import <- c(
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/import?importCategories=&memberStateCodes=&partnerCodes=&products=&sectors=Oilseeds&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/import?importCategories=&memberStateCodes=&partnerCodes=&products=&sectors=Cereals&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/import?importCategories=&memberStateCodes=&partnerCodes=&products=&sectors=Olive%20Oil&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/import?importCategories=&memberStateCodes=&partnerCodes=&products=&sectors=Wine&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/import?importCategories=&memberStateCodes=&partnerCodes=&products=&sectors=Sugar&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/import?importCategories=&memberStateCodes=&partnerCodes=&products=&sectors=Citrus%20Fruit&marketingYears=2023/2024"
-)
+#############################################
+# IMPORTS
+#############################################
 
+# Inicializar vectores
+nombres_archivos_import <- c()
+nombres_url_import <- c()
 
-for(i in 1:length(nombres_url)) {
-  cat("Descargando archivo", i, ":", nombres_archivos[i], "...\n")
+# Construir URLs y nombres de archivo
+for (sector in sectores) {
+  for (ciclo in ciclos_fiscales) {
+    nombre_archivo <- paste0(
+      "import_",
+      gsub("%20", "_", tolower(sector)),
+      "_",
+      gsub("/", "-", ciclo),
+      ".json"
+    )
+
+    url <- paste0(
+      "https://ec.europa.eu/agrifood/api/taxud/weeklyData/import?importCategories=&memberStateCodes=&partnerCodes=&products=&sectors=",
+      sector,
+      "&marketingYears=",
+      ciclo
+    )
+
+    nombres_archivos_import <- c(nombres_archivos_import, nombre_archivo)
+    nombres_url_import <- c(nombres_url_import, url)
+  }
+}
+
+# Descargar archivos
+for (i in seq_along(nombres_url_import)) {
+  cat("Descargando archivo", i, ":", nombres_archivos_import[i], "...\n")
   try({
-    download.file(url = nombres_url[i], 
-                  destfile = nombres_archivos[i], 
-                  mode = "wb", 
-                  quiet = TRUE) # quiet=TRUE reduce el ruido en la consola
+    download.file(
+      url = nombres_url_import[i],
+      destfile = nombres_archivos_import[i],
+      mode = "wb",
+      quiet = TRUE
+    )
   })
 }
 
-# Exports -----------------------------------------------------------------
-
-nombres_archivos_export <- c(
-  "export_oilseeds.json",
-  "export_cereals.json",
-  "export_olive_oil.json",
-  "export_wine.json",
-  "export_sugar.json",
-  "export_citrus_fruit.json"
-)
-
-nombres_url_export <- c(
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/export?memberStateCodes=&partnerCodes=&products=&sectors=Oilseeds&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/export?memberStateCodes=&partnerCodes=&products=&sectors=Cereals&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/export?memberStateCodes=&partnerCodes=&products=&sectors=Olive%20Oil&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/export?memberStateCodes=&partnerCodes=&products=&sectors=Wine&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/export?memberStateCodes=&partnerCodes=&products=&sectors=Sugar&marketingYears=2023/2024",
-  "https://ec.europa.eu/agrifood/api/taxud/weeklyData/export?memberStateCodes=&partnerCodes=&products=&sectors=Citrus%20Fruit&marketingYears=2023/2024"
-)
-
-for(i in 1:length(nombres_url_export)) {
-  cat("Descargando archivo", i, ":", nombres_archivos_export[i], "...\n")
-  try({
-    download.file(url = nombres_url_export[i], 
-                  destfile = nombres_archivos_export[i], 
-                  mode = "wb", 
-                  quiet = TRUE) # quiet=TRUE reduce el ruido en la consola
-  })
-}
-
-
-# Compact the data --------------------------------------------------------
-
-## Imports ----------------------------------------------------------------
-
-
-
+# Leer imports
 dataset_imports <- lapply(nombres_archivos_import, function(archivo) {
   datos <- fromJSON(archivo, flatten = TRUE)
-  
-  # Añadimos columna de origen
-  datos$origen <- archivo 
-  
+  datos$origen <- archivo
+
+  partes <- strsplit(archivo, "_")[[1]]
+  producto_raw <- partes[2]
+  ciclo_raw <- gsub(".json", "", partes[3])
+
+  producto <- toTitleCase(gsub("_", " ", producto_raw))
+  ciclo <- gsub("-", "/", ciclo_raw)
+
+  hora <- format(Sys.time(), "%H:%M:%S")
+
+  cat(sprintf(
+    "[%s] Read %s %s %d imports,\n",
+    hora, producto, ciclo, nrow(datos)
+  ))
+
   return(datos)
-}) |> 
-  bind_rows()
+}) |> bind_rows()
 
-cat("Dimensiones finales:", nrow(dataset_imports), "filas y", ncol(dataset_imports), "columnas.\n")
+cat("Dimensiones finales (imports):", nrow(dataset_imports), "filas y", ncol(dataset_imports), "columnas.\n")
 
-# CSV 
-# write.csv(dataset_imports, "dataset_agrifood_imports.csv", row.names = FALSE)
-
-# Formato R
+dir.create("data", showWarnings = FALSE)
 saveRDS(dataset_imports, "data/dataset_agrifood_imports.rds")
 
 
-## Exports ----------------------------------------------------------------
+#############################################
+# EXPORTS (idéntico a imports)
+#############################################
 
+nombres_archivos_export <- c()
+nombres_url_export <- c()
+
+# Construir URLs y nombres de archivo para EXPORTS
+for (sector in sectores) {
+  for (ciclo in ciclos_fiscales) {
+    nombre_archivo <- paste0(
+      "export_",
+      gsub("%20", "_", tolower(sector)),
+      "_",
+      gsub("/", "-", ciclo),
+      ".json"
+    )
+
+    url <- paste0(
+      "https://ec.europa.eu/agrifood/api/taxud/weeklyData/export?memberStateCodes=&partnerCodes=&products=&sectors=",
+      sector,
+      "&marketingYears=",
+      ciclo
+    )
+
+    nombres_archivos_export <- c(nombres_archivos_export, nombre_archivo)
+    nombres_url_export <- c(nombres_url_export, url)
+  }
+}
+
+# Descargar archivos export
+for (i in seq_along(nombres_url_export)) {
+  cat("Descargando archivo", i, ":", nombres_archivos_export[i], "...\n")
+  try({
+    download.file(
+      url = nombres_url_export[i],
+      destfile = nombres_archivos_export[i],
+      mode = "wb",
+      quiet = TRUE
+    )
+  })
+}
+
+# Leer exports
 dataset_exports <- lapply(nombres_archivos_export, function(archivo) {
   datos <- fromJSON(archivo, flatten = TRUE)
-  
-  # Añadimos columna de origen
-  datos$origen <- archivo 
-  
+  datos$origen <- archivo
+
+  partes <- strsplit(archivo, "_")[[1]]
+  producto_raw <- partes[2]
+  ciclo_raw <- gsub(".json", "", partes[3])
+
+  producto <- toTitleCase(gsub("_", " ", producto_raw))
+  ciclo <- gsub("-", "/", ciclo_raw)
+
+  hora <- format(Sys.time(), "%H:%M:%S")
+
+  cat(sprintf(
+    "[%s] Read %s %s %d exports,\n",
+    hora, producto, ciclo, nrow(datos)
+  ))
+
   return(datos)
-}) |> 
-  bind_rows()
+}) |> bind_rows()
 
-cat("Dimensiones finales:", nrow(dataset_exports), "filas y", ncol(dataset_exports), "columnas.\n")
+cat("Dimensiones finales (exports):", nrow(dataset_exports), "filas y", ncol(dataset_exports), "columnas.\n")
 
-# CSV 
-# write.csv(dataset_exports, "dataset_agrifood_exports.csv", row.names = FALSE)
-
-# Formato R
 saveRDS(dataset_exports, "data/dataset_agrifood_exports.rds")
